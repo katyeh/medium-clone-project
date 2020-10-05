8bcbd632247fffb0de5313927ae7d78940f14e00
Ryan Black <ryanblack045@gmail.com>
2020-10-05 17:11:19 -0400

    working on bookmark model

---
+++ db/migrations/20201005204855-create-bookmark.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Bookmarks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      storyId: {
+        allowNull:false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Stories",
+          key: "id"
+        }
+      },
+      userId: {
+        allowNull:false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Users",
+          key: "id"
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Bookmarks');
+  }
+};
