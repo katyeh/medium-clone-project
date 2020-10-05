38c4812502879371c66c7d94e4f0ddfd6c3d7509
Cole McCoy <cole.mccoy41@hotmail.com>
2020-10-05 17:31:48 -0400

    added follower model

---
+++ db/migrations/20201005212532-create-follower.js
@@ -0,0 +1,34 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Followers', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      followeeId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {model: "User"}
+      },
+      followerId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {model: "User"}
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Followers');
+  }
+};
