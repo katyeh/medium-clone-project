18a4b8b1022a530e585f562a06de9602bc55ced2
Kathleen Yeh <kathleenyeh1@gmail.com>
2020-10-05 14:18:04 -0700

    Fixed responses branch

--- db/migrations/20201005211303-create-response.js
+++ db/migrations/20201005211303-create-response.js
@@ -8,10 +8,6 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      id: {
-        allowNull: false,
-        type: Sequelize.INTEGER
-      },
       body: {
         allowNull: false,
         type: Sequelize.STRING(255)
