7aca332d5ad78b6b602211bd66c086212968517d
Kathleen Yeh <kathleenyeh1@gmail.com>
2020-10-05 14:14:39 -0700

    Created response model.

---
+++ db/migrations/20201005211303-create-response.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Responses', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      id: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      body: {
+        allowNull: false,
+        type: Sequelize.STRING(255)
+      },
+      storyId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: { model: 'Story' }
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: { model: 'User' }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Responses');
+  }
+};
