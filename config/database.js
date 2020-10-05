c88914c8571f7cba9226cfe73ec382324b9ee558
Yuka <yukamoribe@outlook.com>
2020-10-05 18:18:40 -0400

    Fix database.js

--- config/database.js
+++ config/database.js
@@ -1,26 +1,17 @@
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
+const config = require("./index");
+
+const db = config.db;
+const username = db.username;
+const password = db.password;
+const database = db.database;
+const host = db.host;
+
+module.exports = {
+  development: {
+    username,
+    password,
+    database,
+    host,
+    dialect: "postgres",
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
-  }
 }
