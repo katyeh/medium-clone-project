2516bed6f0c0b10669cfa8aa14a151721cc4f233
Kathleen Yeh <kathleenyeh1@gmail.com>
2020-10-06 10:44:56 -0700

    added route for responses

--- routes/api/stories.js
+++ routes/api/stories.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const db = require('../db/models');
 const { asyncHandler, handleValidationErrors } = require("../../utils");
-const { check } = require('express-validator');
-const {User, Story} = require("../../db/models");
+const { check, validationResult } = require('express-validator');
+const {User, Story, Response } = db;
+const express = require('express');
+const router = express.Router();
+const { requireAuth } = require('../auth');
 
-
-
-
-
-
+router.use(requireAuth);
 
 const storyValidator = [
   check('body')
@@ -26,6 +26,23 @@ router.post('/', storyValidator, handleValidationErrors, asyncHandler(async (req
   });
   res.redirect('/stories')
 }))
+
+const validateResponse = [
+    check('body')
+        .exists({ checkFalsy: true })
+        .withMessage("Response can't be empty.")
+        .isLength({ max: 255 })
+        .withMessage("Response can't be longer than 255 characters.")
+];
+
+router.get('/', asyncHandler(async (req, res) => {
+    const responses = await Response.findAll({
+        include: [{ model: User, as: "user", attributes: ["username"] }],
+        order: [["createdAt", "DESC"]],
+        attributes: ["body"],
+    });
+    res.json({ responses });
+}));
 
 
 
