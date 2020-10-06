eaf7ad45412df606da977229063e8369af621e8a
Kathleen Yeh <kathleenyeh1@gmail.com>
2020-10-06 10:07:32 -0700

    responses routes

--- routes/responses.js
+++ routes/responses.js
@@ -1,7 +1,30 @@
 const express = require('express');
 const router = express.Router();
-const { asyncHandler } = require('../db/models');
+const db = require('../db/models');
+const { Response } = db;
+const { check, validationResult } = require('espress-validator');
+const { asyncHandler, handleValidationErrors } = require('../utils');
+const { requireAuth } = require('../auth');
+
+router.use(requireAuth);
+
+const validateResponse = [
+    check('body')
+        .exists({ checkFalsy: true })
+        .withMessage("Response can't be empty.")
+        .isLength({ max: 255 })
+        .withMessage("Response can't be longer than 255 characters.")
+];
 
 router.get('/', asyncHandler(async (req, res) => {
-    const 
+    const responses = await Response.findAll({
+        include: [{ model: User, as: "user", attributes: ["username"] }],
+        order: [["createdAt", "DESC"]],
+        attributes: ["body"],
+    });
+    res.json({ responses });
 }));
+
+const responseNotFound = (id) => {
+    const err = Error(`Response with the `)
+}
