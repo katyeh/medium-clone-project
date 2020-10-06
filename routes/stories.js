7e79cb1656b7fe9e298acbb4330f862501730ff2
Ryan Black <ryanblack045@gmail.com>
2020-10-06 09:36:13 -0400

    added story validator & handleValidationErrors to story route

--- routes/stories.js
+++ routes/stories.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const { asyncHandler } = require("../utils");
+const { asyncHandler, handleValidationErrors } = require("../utils");
+const { check } = require('express-validator');
 const {User, Story} = require("../db/models");
 
 
-router.post('/', asyncHandler(async (req, res, next) => {
+
+
+
+
+
+const storyValidator = [
+  check('body')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a valid story')
+
+]
+
+router.post('/', storyValidator, handleValidationErrors, asyncHandler(async (req, res, next) => {
   const { title, body } = req.body;
   const story = await Story.create({
     title,
