4e7ffb275ab7aba293504c201cca86b2b3e53b5f
Kathleen Yeh <kathleenyeh1@gmail.com>
2020-10-06 11:19:39 -0700

    Added responseNotFound error handler.

--- routes/responses.js
+++ routes/responses.js
@@ -13,4 +13,24 @@ router.get('/', asyncHandler(async (req, res) => {
     res.json({ responses });
 }));
 
+const responseNotFound = (id) => {
+    const err = Error(`Response with the id of ${id} could not be found.`);
+    err.title = "Response not found.";
+    err.status = 404;
+    return err;
+};
+
+router.delete('/:id(\\d+)',
+    asyncHandler(async (req, res, next) => {
+        const responseId = parseInt(req.params.id, 10);
+        const response = await Response.findByPk(responseId);
+
+        if (response) {
+            await response.destroy();
+            res.status(204).end();
+        } else {
+            next(responseNotFound(responseId));
+        }
+    }))
+
 module.exports = router;
