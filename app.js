b654ac1e458823230581b829cfbf72e76fc3eaf0
Cole McCoy <cole.mccoy41@hotmail.com>
2020-10-05 23:24:51 -0400

    added simple follow route handler

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
+const usersRouter = require("./routes/users");
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
+
+app.use("/users", usersRouter);
+
 
 module.exports = app;
