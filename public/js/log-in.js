eb7bcc1d0998a9fb303fb851baf950da2aeeff91
Ryan Black <ryanblack045@gmail.com>
2020-10-08 16:12:04 -0400

    working through error handler

--- public/js/log-in.js
+++ public/js/log-in.js
@@ -30,5 +30,33 @@ logInForm.addEventListener("submit", async (e) => {
 
     window.location.href = "/login"
   } catch (err) {
- 
+    if (err.status >= 400 && err.status < 600) {
+    const errorJSON = await err.json();
+    const errorsContainer = document.querySelector(".errors-container");
+    let errorsHtml = [
+      `
+        <div class="alert">
+            Something went wrong. Please try again.
+        </div>
+      `,
+    ];
+    const { errors } = errorJSON;
+    if (errors && Array.isArray(errors)) {
+      errorsHtml = errors.map(
+        (message) => `
+          <div class="alert">
+              ${message}
+          </div>
+        `
+      );
+    }
+    errorsContainer.innerHTML = errorsHtml.join("");
+  } else {
+    alert(
+      "Something went wrong. Please check your internet!"
+    );
+  }
+};
+
+
 })
