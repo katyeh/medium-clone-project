8af57786064c9f4a9c8f076096bf6f8327390d64
Cole McCoy <cole.mccoy41@hotmail.com>
2020-10-09 01:00:07 -0400

    successfully fetched data to profile

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 // const csrfProtection = require('./csrf')
 // const csrf = require('csurf');
-const { Story, Genre } = require('../db/models');
+const { Story, Genre, User } = require('../db/models');
 const { asyncHandler } = require('../utils');
 const router = express.Router();
-const userId = localStorage.getItem("READIUM_CURRENT_USER_ID", id);
-// const csrfProtection = csrf({ cookie: true });
+// const userId = localStorage.getItem("READIUM_CURRENT_USER_ID", id);
+const userId = 1
 
 router.get('/', asyncHandler(async(req, res) => {
   res.render('splash', {
@@ -17,11 +17,14 @@ router.get('/', asyncHandler(async(req, res) => {
 //   res.render('main');
 // });
 
-router.get('/profile', (req, res) => {
-  const stories = await Story.findAll({ where: { userId: userId }})
-  console.log(stories);
-  // res.render('profile');
-})
+router.get('/profile', asyncHandler(async (req, res) => {
+  const stories = await Story.findAll({ where: { userId }})
+  const user = await User.findAll({ where: { id: userId }})
+  res.render('profile', {
+    stories,
+    user
+  });
+}));
 
 
 router.get('/stories/create', asyncHandler(async (req, res) => {
